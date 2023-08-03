1c1c3ed673d000282f3c9acc14207d22feaecf16
Salman Mahmud <salman_mahmud@hotmail.com>
2023-08-02 20:29:10 -0400

    changes to styling and centering

--- todo-list/src/app/task-list/task-list.component.ts
+++ todo-list/src/app/task-list/task-list.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-task-list',
+  selector: 'task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent {
 
   constructor(){
-    
+
   }
 
 
